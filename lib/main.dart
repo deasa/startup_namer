d81bded7a12562180d0dc68673e839b2cd5652ca
Brendan Ashton <bgeorgeashton@gmail.com>
2022-12-22 08:47:20 -0700

    finished codelab1

---
+++ lib/main.dart
@@ -0,0 +1,85 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: RandomWords(),
+      );
+  }
+}
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      /*
+        The itemBuilder callback is called once per suggested word paring, and
+        places each suggestion into a ListTitle row. For even rows, the function
+        adds a ListTitle row for the word pairing. For odd rows, the function
+        adds a Divider widget to visually separate the entries. Note that the
+        divider may be difficult to see on smaller devices.
+       */
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          // Add a one-pixel-high divider widget in the ListView
+          return Divider();
+        }
+
+        /*
+          The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+          For example: 1,2,3,4,5 becomes 0,1,1,2,2.
+          This calculates the actual number of word pairings in the ListView,
+          minus the divider widgets.
+         */
+
+        final int index = i ~/ 2;
+
+        // If you've reached the end of the available word pairings...
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+    }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+}
