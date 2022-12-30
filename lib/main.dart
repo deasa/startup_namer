d248b63de69fbeb44b88926815774efdf17be86d
Brendan Ashton <bgeorgeashton@gmail.com>
2022-12-30 07:42:24 -0700

    replace main.dart with 'https://github.com/flutter/codelabs/blob/master/startup_namer/step4_infinite_list/lib/main.dart'

--- lib/main.dart
+++ lib/main.dart
@@ -9,77 +9,68 @@ void main() {
   runApp(const MyApp());
 }
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords(),
-      );
+      title: 'Startup Name Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Startup Name Generator'),
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      ),
+    );
   }
+// #enddocregion build
 }
-class RandomWords extends StatefulWidget {
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
+// #enddocregion MyApp
 
+// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18);
+  // #enddocregion RWS-var
 
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
+    // #docregion itemBuilder
     return ListView.builder(
-      padding: const EdgeInsets.all(16),
-      /*
-        The itemBuilder callback is called once per suggested word paring, and
-        places each suggestion into a ListTitle row. For even rows, the function
-        adds a ListTitle row for the word pairing. For odd rows, the function
-        adds a Divider widget to visually separate the entries. Note that the
-        divider may be difficult to see on smaller devices.
-       */
-      itemBuilder: (BuildContext _context, int i) {
-        if (i.isOdd) {
-          // Add a one-pixel-high divider widget in the ListView
-          return Divider();
-        }
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
 
-        /*
-          The syntax "i ~/ 2" divides i by 2 and returns an integer result.
-          For example: 1,2,3,4,5 becomes 0,1,1,2,2.
-          This calculates the actual number of word pairings in the ListView,
-          minus the divider widgets.
-         */
-
-        final int index = i ~/ 2;
-
-        // If you've reached the end of the available word pairings...
+        final index = i ~/ 2; /*3*/
         if (index >= _suggestions.length) {
-          // ...then generate 10 more and add them to the suggestions list.
-          _suggestions.addAll(generateWordPairs().take(10));
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
-        return _buildRow(_suggestions[index]);
-    }
+        // #docregion listTile
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+        // #enddocregion listTile
+      },
     );
+    // #enddocregion itemBuilder
   }
+// #enddocregion RWS-build
+// #docregion RWS-var
+}
+// #enddocregion RWS-var
 
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-    );
-  }
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
 }
